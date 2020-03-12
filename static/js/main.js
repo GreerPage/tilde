0a15d575f361ba8dead9e4bb0ccf99a6670922cf
GreerPage <greerishere@gmail.com>
2020-03-12 19:42:16 -0400

    lots

--- static/js/main.js
+++ static/js/main.js
@@ -1,13 +1,19 @@
 $(document).ready(function() {
     $("#first").click(function(){
         $("#id_img").click(); 
+        $("#first").hide();
+        $('#next-button').attr('style', 'visible');
+        return false;
+    });
+    $('#post-botton-submit-yeah').click(function(){
+        $('#post-botton-submit').click();
         return false;
     });
     $("#post-button").click(function(){
         $("#post-botton-submit").click(); 
         return false;
     });
-    $('#image').on('load', function () {
+    $('#next-button').click(function () {
         $('#image').cropper({
             aspectRatio: 16 / 16,
             zoomable: false,
@@ -16,6 +22,8 @@ $(document).ready(function() {
                 
             }
         });
+        $('#next-button').hide();
+        $('#done-button').attr('style', 'visible');
     });
     $('#done-button').click(function(){
         var cropData = $('#image').cropper("getData");
@@ -23,15 +31,10 @@ $(document).ready(function() {
         $("#id_y").val(cropData["y"]);
         $("#id_height").val(cropData["height"]);
         $("#id_width").val(cropData["width"]);
-        $('#to-hide').hide()
-    });
-    $('#post-button').click(function(){
-        var cropData = $image.cropper("getData");
-        $("#id_x").val(cropData["x"]);
-        $("#id_y").val(cropData["y"]);
-        $("#id_height").val(cropData["height"]);
-        $("#id_width").val(cropData["width"]);
-        $("#post-button").submit();
+        $('#to-hide').hide();
+        $('#done-button').hide();
+        $('#preview').attr('style', 'visibility: visible');
+        $('#post-botton-submit-yeah').attr('style', 'visibility: visible;');
     });
     $('#id_img').attr('onChange', 'img_pathUrl(this);')
     $('#imgformW').on('submit', function(event){
