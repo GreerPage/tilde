ff2a243e7726b6274936c21ecc4270e18435a425
GreerPage <greerishere@gmail.com>
2020-03-12 17:32:57 -0400

    ?

--- tilde/settings.py
+++ tilde/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '5(gst$2e44_u0k+@rn@k=93%h&jrs0v@q1x*#k#&x90dj!x@oc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
