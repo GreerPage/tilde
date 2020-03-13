e13049adaf28c627df635d6d033066d9a9397fc1
Greer Page <45717306+GreerPage@users.noreply.github.com>
2020-03-13 17:31:51 -0400

    Update models.py

--- post/models.py
+++ post/models.py
@@ -2,7 +2,7 @@ from django.db import models
 import random
 from django.conf import settings
 import os
-from PIL import Image
+from PIL import Image, ExifTags
 
 # Create your models here.
 def generateName(instance, filename):
@@ -14,9 +14,19 @@ def generateName(instance, filename):
 def cropper(original_path, x, y, height, width):
     original_path = str(original_path)
     img = Image.open(os.path.join(settings.BASE_DIR, 'media/'+original_path))
-    img = img.load()
+    try:
+        exif=dict((ExifTags.TAGS[k], v) for k, v in img._getexif().items() if k in ExifTags.TAGS)
+        print(exif)
+        if exif['Orientation']==6:
+            img=img.rotate(270, expand=True)
+        if exif['Orientation']==8:
+            img=img.rotate(90, expand=True)
+        if   exif['Orientation']==3: 
+            img=img.rotate(180, expand=True)
+    except:
+        pass
     cropped_image = img.crop((x, y, width+x, height+y))
-    resized_image = cropped_image.resize((400, 400), Image.ANTIALIAS)
+    resized_image = cropped_image.resize((800, 800), Image.ANTIALIAS)
     resized_image.save(os.path.join(settings.BASE_DIR, 'media/'+ generateName(0, original_path)))
     os.remove(os.path.join(settings.BASE_DIR, 'media/'+original_path))
 
