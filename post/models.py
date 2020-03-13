16534f946bf41c854e1f1728f78a19fa0b747aa3
GreerPage <greerishere@gmail.com>
2020-03-12 21:47:41 -0400

    gallery

--- post/models.py
+++ post/models.py
@@ -8,7 +8,7 @@ from PIL import Image
 def generateName(instance, filename):
     basefilename, file_extension= os.path.splitext(filename)
     chars= 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890'
-    randomstr= ''.join((random.choice(chars)) for x in range(100))
+    randomstr= ''.join((random.choice(chars)) for x in range(20))
     return 'images/{randomstring}{ext}'.format(randomstring= randomstr, ext= file_extension)    
 
 def cropper(original_path, x, y, height, width):
