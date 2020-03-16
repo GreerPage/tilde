23151c42954c7192c51e4db380c9f9f580988269
GreerPage <greerishere@gmail.com>
2020-03-16 19:28:04 -0400

    made show more button

--- static/js/display.js
+++ static/js/display.js
@@ -2,13 +2,13 @@ function displayPostCaption() {
     height = $('#caption-for-posts').css('height');
     if(height === '40px') {
         $('#caption-for-posts').css('height', 'auto');
-        $('#caption-button').html('less')
+        $('#caption-button').html('show less')
         $('#less').css('color', '#757575')
         $('#more-text').hide()
     }
     else {
         $('#caption-for-posts').css('height', '40px');
-        $('#caption-button').html('...')
+        $('#caption-button').html('show more')
         $('#less').css('color', '#313131')
         $('#more-text').show()
     }
